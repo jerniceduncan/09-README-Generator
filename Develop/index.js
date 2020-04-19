f6a4fd0a54ae67e44777bd08f56ba5ad71f971a3
jerniceduncangit config --global user.name git config --global user.name :jerniceduncan <jerniceduncan@icloud.com>
2020-04-19 16:09:26 -0700

    Init Repo

---
+++ Develop/index.js
@@ -0,0 +1,29 @@
+// create a variable and require
+var questions = require("questions");
+
+// create questions array
+const questions = [
+    'askProjectName',
+    'askProjectVersion',
+    'askProjectDescription',
+    'askProjectHomepage',
+    'askProjectDemoUrl',
+    'askAuthorName',
+    'askAuthorGithub'
+    'askLicenseName',
+    'askContributingUrl',
+    'askInstallCommand',
+    'askUsage',
+    'askTestCommand'
+
+];
+// 
+
+function writeToFile(index.js, 'questions') {
+}
+
+function init() {
+
+}
+
+init();
