b373ba9495111e6d03244943321a03cf5f6ed904
Jernice Butts Duncan <jerniceduncan@icloud.com>
2020-05-13 01:20:03 -0700

    updated promise

---
+++ Develop/README.md
@@ -0,0 +1,24 @@
+
+  # README.md_Generator
+  my_email@github.com
+
+  https://avatars3.githubusercontent.com/u/11791361?v=4
+
+# readme
+
+# 
+
+# 
+
+# 
+
+# Apache License 2.0
+
+# 
+
+# 
+
+# undefined
+
+[![badge](https://img.shields.io/badge/Github-Project-brightgreen)
+
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,15 +1,16 @@
-const axios = require('axios');
+const axios = require("axios");
 
 
-const api = {
-  getUser(username) {
-const queryUrl = `https://api.github.com/users/${username}`;
-axios.get(queryUrl).then(function(response){
-  return response.data.avatar_url;
-})
+function apiCall(username) {
+  const queryUrl = `https://api.github.com/users/${username}`;
 
+  return axios.get(queryUrl).then(function (response) {
+    const result = {
+      avatar_url: response.data.avatar_url,
+      email: response.data.email ? response.email : "my_email@github.com",
+    };
+    return result;
+  });
+}
 
-  }
-};
-
-module.exports = api;
+module.exports = apiCall;
