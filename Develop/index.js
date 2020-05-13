9fdd153b0cd74360309d6061745bff90219e33bb
Jernice Butts Duncan <jerniceduncan@icloud.com>
2020-05-12 22:37:34 -0700

    updated inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,71 @@
 // create a variable and require
-var questions = require("questions");
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
+const api = require("./utils/api")
 
 // create questions array
+// copy and paste type, name , message add question inquirer
 const questions = [
-    'askProjectName',
-    'askProjectVersion',
-    'askProjectDescription',
-    'askProjectHomepage',
-    'askProjectDemoUrl',
-    'askAuthorName',
-    'askAuthorGithub'
-    'askLicenseName',
-    'askContributingUrl',
-    'askInstallCommand',
-    'askUsage',
-    'askTestCommand'
+    {
+        type: 'input',
+        name: 'askProjectName',
+        message: 'Enter a project title?'
+    },
+    {
+        type: 'input',
+        name: 'askGitHubUserName',
+        message: 'What is your Github Username?'
+    },
+    {
+        type: 'input',
+        name: 'askProjectName',
+        message: 'Enter a project title?'
+    },
+    {
+        type: 'input',
+        name: 'askProjectName',
+        message: 'Enter a project title?'
+    },
+    {
+        type: 'input',
+        name: 'askProjectName',
+        message: 'Enter a project title?'
+    },
+
+    // 'askProjectVersion',
+    // 'askProjectDescription',
+    // 'askProjectHomepage',
+    // 'askProjectDemoUrl',
+    // 'askAuthorName',
+    // 'askAuthorGithub'
+    // 'askLicenseName',
+    // 'askContributingUrl',
+    // 'askInstallCommand',
+    // 'askUsage',
+    // 'askTestCommand'
 
 ];
 // 
 
-function writeToFile(index.js, 'questions') {
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(error) =>{
+        if (error) throw error;
+        console.log('readme created sucessesfully');
+    })
 }
 
 function init() {
-
+    inquirer.prompt(questions).then(answers=> {
+        console.log("Answers...", answers);
+    let data = {...answers}
+        const avatarPhoto = api(answers.askGitHubUserName);
+        console.log(avatarPhoto)
+        data.photo = avatarPhoto;
+        const markDown = generateMarkDown (data);
+        writeToFile("README.md", markdown);
+    });
 }
 
 init();
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,5 +1,13 @@
+const axios = require('axios');
+
+
 const api = {
   getUser(username) {
+const queryUrl = `https://api.github.com/users/${username}`;
+axios.get(queryUrl).then(function(response){
+  return response.data.avatar_url;
+})
+
 
   }
 };
